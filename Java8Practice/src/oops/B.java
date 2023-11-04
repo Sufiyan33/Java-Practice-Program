c43c6a86f99ca048e8507cdb0e2fd99bc77175d7
Sufiyan33 <Basu700736@gmail.com>
2023-11-04 12:55:50 +0530

    Calling method using super keyword

--- Java8Practice/src/oops/B.java
+++ Java8Practice/src/oops/B.java
@@ -4,5 +4,11 @@ public class B extends A {
 
 	public void display() {
 		System.out.println("Shuold dispplay method of class B");
+		super.display();
+	}
+
+	public static void main(String[] args) {
+		A a = new B();
+		a.display();
 	}
 }
