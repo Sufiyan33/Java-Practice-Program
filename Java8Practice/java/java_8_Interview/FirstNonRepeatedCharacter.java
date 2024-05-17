55f6cf2829f4ac440ac41c0fe368a33e9b21e5ea
Sufiyan33 <basu700736@gmail.com>
2024-05-17 19:49:47 +0530

    Instruction to solve problem

--- Java8Practice/java/java_8_Interview/FirstNonRepeatedCharacter.java
+++ Java8Practice/java/java_8_Interview/FirstNonRepeatedCharacter.java
@@ -6,6 +6,11 @@ import java.util.stream.Collectors;
 
 public class FirstNonRepeatedCharacter {
 
+	/*
+	 * Find stream from string... (example :: input.chars()) 1 - First convert
+	 * string into Character object. 2 - Then find identity and count all characters
+	 * then store in LinkedHashMap so that avoid duplicate.
+	 */
 	public static void main(String[] args) {
 		String input = "Java articles are Awesome";
 		input.chars().mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
