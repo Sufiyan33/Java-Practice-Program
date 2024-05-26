cefcce92ee67a2ef93e2efa40c3daab134f9632f
Sufiyan33 <basu700736@gmail.com>
2024-05-26 15:19:12 +0530

    Find Number with even digits approach 1

---
+++ Java8Practice/java/linerSearch/FindNumberWithEvenDigits.java
@@ -0,0 +1,32 @@
+package linerSearch;
+
+public class FindNumberWithEvenDigits {
+
+	/*
+	 * Here, in the main task is to find out number with even digits. ex
+	 * [12,123,1,23,12], here 12 has even digit number and 1 or 123 have odd digit
+	 * numbers.
+	 */
+	// Approach :
+
+	/*
+	 * 1 : Count number of digits.
+	 * 
+	 * 2 : Convert number in string and find its length.
+	 */
+	public static void main(String[] args) {
+		int[] arr = { 12, 345, 26, 6, 5, 7896, 23, 13 };
+
+		int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			String num = arr[i] + "";
+			int numLength = num.length();
+
+			if (numLength % 2 == 0) {
+				count++;
+			}
+		}
+		System.out.println("Even digits are :: " + count);
+	}
+}
