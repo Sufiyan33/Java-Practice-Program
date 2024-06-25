ee000961cece57287d9b927f13fe6cabca657840
Sufiyan33 <basu700736@gmail.com>
2024-06-25 23:05:16 +0530

    Test knowledge

---
+++ Java8Practice/java8/interview/Bas.java
@@ -0,0 +1,17 @@
+package interview;
+
+public class Bas extends Sup {
+
+	public Bas(String text) {
+		// Here you need to call super class constructor. otherwise it will give
+		// compilation issue.
+
+		super(text);
+		i = 2;
+	}
+
+	public static void main(String[] args) {
+		Bas b = new Bas("Hello");
+		System.out.println(b.i);
+	}
+}
---
+++ Java8Practice/java8/interview/Sup.java
@@ -0,0 +1,10 @@
+package interview;
+
+public class Sup {
+
+	int i = 0;
+
+	public Sup(String text) {
+		i = 1;
+	}
+}
